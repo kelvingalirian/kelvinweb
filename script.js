65278ba4e119a031891c2989be6405cbe67dc6d0
kelvingalirian <167989120+kelvingalirian@users.noreply.github.com>
2024-08-25 20:55:56 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function klikini() {
+    
+    const pesanSapa = `aku mau nanya sesuatu nih kak. Kalau kakak mau tau sesuatu, tinju lagi tombol dibawah.`;
+    document.getElementById("pesanSapa").innerText = pesanSapa;
+}
+
+function mintaNomorWA() {
+    
+    const pesanNomorWA = `boleh minta nomer WhatsApp-nya nggak kak? wkwkwwkkwkwkwkwkkwk`;
+    document.getElementById("nomorWA").innerText = pesanNomorWA;
+}
